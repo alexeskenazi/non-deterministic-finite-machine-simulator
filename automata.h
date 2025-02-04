e9755cbeb624a89b47efef17d071ead1155f1f8f
alexeskenazi <112673600+alexeskenazi@users.noreply.github.com>
2025-02-04 14:40:43 -0500

    Added accept and reject outputs

--- automata.h
+++ automata.h
@@ -50,7 +50,8 @@ class State {
 class Automata {
     public:
     vector<State> states;
-    int currStateId = -1;
+    vector<int> currStateIds;
+    vector<int> newStateIds;
     string input;
     bool debug;
 
@@ -63,38 +64,71 @@ class Automata {
         string output;
 
         // Find the start state and set currState to it.
+        int startStateId = -1;
         for(size_t i = 0; i < states.size(); ++i){
             if(states[i].start){
-                currStateId = i;
+                startStateId = i;
                 break;
             }
         }
 
-        if(debug) cout << "Start state: " << currStateId << endl;
+        if(debug) cout << "Start state: " << startStateId << endl;
 
-        State currState = states[currStateId];
+        // Set initial state
+        currStateIds.push_back(startStateId);
+
+        // Loop through the chars in the input
         for(size_t i = 0; i < input.size(); ++i){
             char c = input[i];
-            for(size_t j = 0; j < currState.transitions.size(); ++j){
-                if(currState.transitions[j]->x == c){
-                    int newStateId  = currState.transitions[j]->q;
-                    if(debug) cout << "on char: " << c << "(" << currState.transitions[j]->x << ")" << " " << currStateId << " -> " << newStateId << endl;
-                    currStateId = newStateId;
-                    currState = states[currStateId];
-                    break;
+
+            for(size_t k = 0; k < currStateIds.size(); ++k){
+                int stateId = currStateIds[k];
+                State currState = states[stateId];
+
+                // Loop through the transitions avaiable for the current state
+                for(size_t j = 0; j < currState.transitions.size(); ++j){
+
+                    // if the transition matches the input char
+                    if(currState.transitions[j]->x == c){
+                        // find the next state for the transition
+                        int newStateId  = currState.transitions[j]->q;
+
+                        newStateIds.push_back(newStateId);
+
+                        if(debug) cout << "on char: " << c << "(" << currState.transitions[j]->x << ")" << " " << stateId << " -> " << newStateId << endl;
+                    
+                    }
                 }
+            }
+            currStateIds = newStateIds;
+        }
+
+        vector<int> acceptedList;
+        vector<int> rejectedList;
+        for(size_t i = 0; i < currStateIds.size(); ++i){
+            int stateId = currStateIds[i];
+            State currState = states[stateId];
+            if(currState.accept){  
+                acceptedList.push_back(stateId);
+            } else {  
+                rejectedList.push_back(stateId);
             }
         }
 
-        if(currState.accept){  
-            return "accept " + to_string(currStateId);
-        } else {  
-            return "reject " + to_string(currStateId);
+        if(acceptedList.size()>0) {
+            string output = "accepted";
+            for(size_t i = 0; i<acceptedList.size(); ++i) {
+                output += " " + to_string(acceptedList[i]);
+            }
+            return output;
+        } else {
+            string output = "rejected";
+            for(size_t i = 0; i<rejectedList.size(); ++i) {
+                output += " " + to_string(rejectedList[i]);
+            }
+            return output;
         }
-        
 
-
-        return output;
     }
 
 };
